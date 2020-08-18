dc156bee45fa8536418389430a787f59f0758419
Todd Butters <todd@ctera.com>
2020-08-18 17:12:10 -0400

    Modified usage instructions. Added logic to check for entities_version which does not exist prior to 6.0.712.

--- convert-backup-to-cloud.sh
+++ convert-backup-to-cloud.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# Script to convert a Cloud Backup folder to a Cloud Drive folder
+# Requires CTERA Portal version 6.0.512.2+ and CTERA Gateway/Filer 6.0.537+.
+# Usage example: sh convert-backup-to-cloud.sh vgateway6b7c 'vgateway6b7c Cloud Folder'
+
 if [ "$#" -ne 2 ]
 then
-  echo "Usage: $0 [-b <backup_folder>] [-c <requested_cloud_folder_name>]"
+  echo "Usage: $0 <BackupFolderName> <'Requested Cloud Folder Name'>"
   exit 1
 fi
 
@@ -13,7 +17,7 @@ declare portal_id
 backupFolderUID=$(/usr/local/ctera/postgres/bin/psql -U postgres -qtAX -c "SELECT bu.uid FROM backups bu JOIN base_objects b ON bu.uid = b.uid WHERE b.name='${backupFolderName}'")
 if [ -z "${backupFolderUID}" ];
 then
-	echo "Wrong backup folder name supplied.. backup folder doesn't exist"
+	echo "Wrong backup folder name supplied. The backup folder '$1' doesn't exist."
   exit 1
 fi
 ownerRes=$(/usr/local/ctera/postgres/bin/psql -U postgres -qtAX -c "select owner_id from base_objects where uid = $backupFolderUID")
@@ -33,7 +37,19 @@ update base_objects set name='${cloudFolderName}' where uid = $backupFolderUID;
 delete from devices_to_folders where folder_id = $backupFolderUID;
 delete from backups where uid = $backupFolderUID;
 insert into cloud_drives values ($backupFolderUID,null,null);
-commit;
-Insert into entities_version (uid,type,state,portal_id,version,update_time) values ($backupFolderUID,1,1,$portal_id,nextval('entity_version_sequence'), now());"
+commit;"
+echo 'Conversion completed. Performing post-check...'
+# Required for portal versions 6.0.712 and higher.
+# Check if entities_version table exists. 
+# If it exists, insert $backupFolderUID into it so devices will see the cloud drive folder after conversion.
+table_exists=$(~postgres/bin/psql -t -c "SELECT EXISTS ( SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'entities_version' );" -U postgres)
 
+if [ $table_exists = 't' ]; then
+  echo "Adding $backupFolderName to entities_version table."
+  ~postgres/bin/psql -U postgres -qtAX -c "INSERT INTO entities_version (uid,type,state,portal_id,version,update_time) values ($backupFolderUID,1,1,$portal_id,nextval('entity_version_sequence'), now());"
+else
+  echo 'The entities_version table does not exist. No more actions required.'
+fi
+
+echo "Finished."
 exit 0
