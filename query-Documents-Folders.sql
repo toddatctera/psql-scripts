50f8b1b216723c38605c2247fa14a02410a5106a
Todd Butters <todd@ctera.com>
2021-03-24 16:01:21 -0400

    Improved usage instructions and removed commented code

--- query-Documents-Folders.sql
+++ query-Documents-Folders.sql
@@ -1,5 +1,15 @@
--- Usage: save as a file, e.g. query-Documents-Folders.sql, and run command below as root.
--- ~postgres/bin/psql -U postgres -f query-Documents-Folders.sql > $datadir/query-results.txt
+/* 
+__Description__
+Select each Cloud Drive Folders named 'Documents' or 'My Documents'.
+Show the folder owner's name, total size, number of files and modified date.
+Number of files is just files, does not count folders.
+__Usage__
+Save as a file, e.g. query-Documents-Folders.sql
+Then run command below as root.
+~postgres/bin/psql -U postgres -f query-Documents-Folders.sql
+Or to save as a CSV and open in Excel or some format.
+~postgres/bin/psql -U postgres --csv -f query-Documents-Folders.sql > $datadir/query-results.csv
+*/
 SELECT 
 	bo.name AS owner_name,subquery.*
 FROM (
@@ -17,7 +27,6 @@ FROM
 		AND bo.type = 'com.ctera.db.objects.CloudDrive'
 WHERE 
 	bo.name LIKE 'Documents' OR bo.name LIKE 'My Documents'
-	--AND bo.type = 'com.ctera.db.objects.CloudDrive'
 	AND fs.is_deleted='f'
 ) as subquery
 JOIN base_objects AS bo ON subquery.owner_id = bo.uid
